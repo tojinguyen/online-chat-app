6f885e1008ee2f8aafed5d06984ce4f9fdedb67a
tojinguyen <toainguyenprogramminggame@gmail.com>
2025-05-06 23:31:25 +0700

    update: next config image cloudinary

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,9 @@ const nextConfig: NextConfig = {
   env: {
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
   },
+  images: {
+    domains: ["res.cloudinary.com"],
+  },
 };
 
 export default nextConfig;
